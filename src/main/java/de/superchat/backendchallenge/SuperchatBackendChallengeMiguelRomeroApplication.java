4f7179e13cd0d297234942f18bb02455e1b32f7e
Miguel Angel Romero <miguelromero717@gmail.com>
2022-03-03 18:21:08 +0100

    Initial Commit

---
+++ src/main/java/de/superchat/backendchallenge/SuperchatBackendChallengeMiguelRomeroApplication.java
@@ -0,0 +1,13 @@
+package de.superchat.backendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperchatBackendChallengeMiguelRomeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperchatBackendChallengeMiguelRomeroApplication.class, args);
+    }
+
+}
